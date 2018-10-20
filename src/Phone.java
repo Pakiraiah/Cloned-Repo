06fc75db247bd092ebac43f6f3d7659a46b5c172
Pakiraiah <ppakiraiah@gmail.com>
2018-10-20 16:18:46 +1100

    commiting

---
+++ src/Phone.java
@@ -0,0 +1,14 @@
+
+public class Phone {
+	
+	public void call(){
+		
+		System.out.println("Phone calling");
+		
+	}
+	
+	public void voiceText(){
+		System.out.println("Sending Text");
+	}
+
+}
---
+++ src/TestPhone.java
@@ -0,0 +1,13 @@
+
+public class TestPhone {
+
+	public static void main(String[] args) {
+	
+		Phone p = new Phone();
+		
+		p.call();
+		p.voiceText();
+
+	}
+
+}
